03abe505f5daca02d6290f819291d7c67eb1b7b7
KuligKamil <kamil.marek.kulig@gmail.com>
2024-10-30 23:13:45 +0100

    chore: work on filter words

---
+++ backend/src/crud.py
@@ -0,0 +1,70 @@
+from typing import Optional
+from src.models import Language, PartOfSpeech, Word
+
+
+# async def add_word(
+#     value: str,
+#     meaning: str,
+#     is_learned: bool,
+#     language: Language = Language.spanish,
+#     part_of_speech: PartOfSpeech = PartOfSpeech.verb,
+# ) -> Word:
+#     return await Word.insert(
+#         Word(
+#             value=value,
+#             meaning=meaning,
+#             is_learned=is_learned,
+#             language=language,
+#             part_of_speech=part_of_speech,
+#         )
+#     )
+
+
+# async def update_word(
+#     id: str,
+#     value: str,
+#     meaning: str,
+#     is_learned: bool,
+#     language: Language = Language.spanish,
+#     part_of_speech: PartOfSpeech = PartOfSpeech.verb,
+# ) -> Word:
+#     word = await Word.find_one(Word.id == id)
+#     word.value = value
+#     word.meaning = meaning
+#     word.is_learned = is_learned
+#     word.language = language  # maybe not useful
+#     word.part_of_speech = part_of_speech
+#     await word.save()
+#     return word
+
+
+# async def delete_word(
+#     id: str,
+# ) -> Word:
+#     await Word.find_one(Word.id == id).delete()
+
+
+# async def get_word(
+#     id: str,
+# ) -> Word:
+#     return await Word.find_one(Word.id == id)
+
+
+async def get_words(
+    value: Optional[str] = None,
+    # meaning: str,
+    is_learned: Optional[bool] = None,
+    language: Optional[Language] = None,
+    part_of_speech: Optional[PartOfSpeech] = None,
+) -> list[Word]:
+    search_criteria = {}
+    if isinstance(is_learned, bool):
+        search_criteria["is_learned"] = is_learned
+    if value is not None:
+        search_criteria["value"] = {"$regex": value, "$options": "i"}
+    if language is not None:
+        search_criteria["language"] = language
+    if part_of_speech is not None:
+        search_criteria["part_of_speech"] = part_of_speech
+    print(search_criteria)
+    return await Word.find(search_criteria).to_list()
--- backend/src/main.py
+++ backend/src/main.py
@@ -1,21 +1,54 @@
 from asyncio import run
 
-from database_connection import database_init
-from models import Word, Language, PartOfSpeech
+from src.crud import get_words
+from src.database_connection import database_init
+from src.models import Word, Language, PartOfSpeech
 
 
 async def init():
     await database_init(document_models=[Word], clean_database=True)
+
     words = [
+        Word(
+            value="amigo",
+            meaning="friend",
+            is_learned=True,
+            language=Language.spanish,
+            part_of_speech=PartOfSpeech.noun,
+        ),
         Word(
             value="hablar",
             meaning="talk",
             is_learned=True,
             language=Language.spanish,
             part_of_speech=PartOfSpeech.verb,
-        )
+        ),
+        Word(
+            value="hacer",
+            meaning="do",
+            is_learned=False,
+            language=Language.spanish,
+            part_of_speech=PartOfSpeech.verb,
+        ),
     ]
     words_ids = await Word.insert_many(words)
+    # words = await Word.find({"is_learned": True}).to_list()
+    # words = await ws()
+    words = await get_words(is_learned=False)
+    print(len(words))
+    assert len(words) == 1
+
+    words = await get_words(part_of_speech=PartOfSpeech.noun)
+    print(len(words))
+    assert len(words) == 1
+
+    words = await get_words()
+    print(len(words))
+    assert len(words) == 3
+
+    words = await get_words(value="ami")
+    print(len(words))
+    assert len(words) == 1
 
 
 if __name__ == "__main__":
