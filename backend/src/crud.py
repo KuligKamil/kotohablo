be2b9a78bd8770aa03ce6cf76987f6f5a406261d
KuligKamil <kamil.marek.kulig@gmail.com>
2024-10-31 23:03:48 +0100

    chore: finish first version for crud

--- backend/src/crud.py
+++ backend/src/crud.py
@@ -1,47 +1,50 @@
 from typing import Optional
+
+from bson import Regex
+from pymongo import DESCENDING
 from src.models import Language, PartOfSpeech, Word
 
 
-# async def add_word(
-#     value: str,
-#     meaning: str,
-#     is_learned: bool,
-#     language: Language = Language.spanish,
-#     part_of_speech: PartOfSpeech = PartOfSpeech.verb,
-# ) -> Word:
-#     return await Word.insert(
-#         Word(
-#             value=value,
-#             meaning=meaning,
-#             is_learned=is_learned,
-#             language=language,
-#             part_of_speech=part_of_speech,
-#         )
-#     )
+async def add_word(
+    value: str,
+    meaning: str,
+    is_learned: bool,
+    language: Language = Language.spanish,
+    part_of_speech: PartOfSpeech = PartOfSpeech.verb,
+) -> Word:
+    return await Word.insert(
+        Word(
+            value=value,
+            meaning=meaning,
+            is_learned=is_learned,
+            language=language,
+            part_of_speech=part_of_speech,
+        )
+    )
 
 
-# async def update_word(
-#     id: str,
-#     value: str,
-#     meaning: str,
-#     is_learned: bool,
-#     language: Language = Language.spanish,
-#     part_of_speech: PartOfSpeech = PartOfSpeech.verb,
-# ) -> Word:
-#     word = await Word.find_one(Word.id == id)
-#     word.value = value
-#     word.meaning = meaning
-#     word.is_learned = is_learned
-#     word.language = language  # maybe not useful
-#     word.part_of_speech = part_of_speech
-#     await word.save()
-#     return word
+async def update_word(
+    id: str,
+    value: str,
+    meaning: str,
+    is_learned: bool,
+    language: Language = Language.spanish,
+    part_of_speech: PartOfSpeech = PartOfSpeech.verb,
+) -> Word:
+    word = await Word.find_one(Word.id == id)
+    word.value = value
+    word.meaning = meaning
+    word.is_learned = is_learned
+    word.language = language  # maybe not useful
+    word.part_of_speech = part_of_speech
+    await word.save()
+    return word
 
 
-# async def delete_word(
-#     id: str,
-# ) -> Word:
-#     await Word.find_one(Word.id == id).delete()
+async def remove_word(
+    id: str,
+) -> Word:
+    await Word.find_one(Word.id == id).delete()
 
 
 # async def get_word(
@@ -62,9 +65,11 @@ async def get_words(
         search_criteria["is_learned"] = is_learned
     if value is not None:
         search_criteria["value"] = {"$regex": value, "$options": "i"}
+        # search_criteria["value"] = Regex(pattern=value, flags="i")
     if language is not None:
         search_criteria["language"] = language
     if part_of_speech is not None:
         search_criteria["part_of_speech"] = part_of_speech
-    print(search_criteria)
-    return await Word.find(search_criteria).to_list()
+    return (
+        await Word.find(search_criteria).sort((Word.create_date, DESCENDING)).to_list()
+    )
--- backend/src/main.py
+++ backend/src/main.py
@@ -1,6 +1,6 @@
 from asyncio import run
 
-from src.crud import get_words
+from src.crud import add_word, get_words, remove_word, update_word
 from src.database_connection import database_init
 from src.models import Word, Language, PartOfSpeech
 
@@ -34,22 +34,47 @@ async def init():
     words_ids = await Word.insert_many(words)
     # words = await Word.find({"is_learned": True}).to_list()
     # words = await ws()
+
     words = await get_words(is_learned=False)
-    print(len(words))
     assert len(words) == 1
 
     words = await get_words(part_of_speech=PartOfSpeech.noun)
-    print(len(words))
     assert len(words) == 1
 
     words = await get_words()
-    print(len(words))
     assert len(words) == 3
 
     words = await get_words(value="ami")
-    print(len(words))
     assert len(words) == 1
 
+    word = await add_word(
+        value="correr",
+        meaning="run",
+        is_learned=True,
+        language=Language.spanish,
+        part_of_speech=PartOfSpeech.verb,
+    )
 
+    words = await get_words()
+    assert len(words) == 4
+
+    word = await update_word(
+        id=word.id,
+        value="gordo",
+        meaning="fat",
+        is_learned=False,
+        language=Language.spanish,
+        part_of_speech=PartOfSpeech.adjective,
+    )
+    assert (
+        word.value == "gordo"
+        and word.meaning == "fat"
+        and word.is_learned == False
+        and word.part_of_speech == PartOfSpeech.adjective
+    )
+
+    await remove_word(id=word.id)
+    words = await get_words()
+    assert len(words) == 3
 if __name__ == "__main__":
     run(init())
